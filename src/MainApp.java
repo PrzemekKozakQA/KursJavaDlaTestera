eb5ab9d8e63f5c735fd57da54ecb1dee1bc643d9
PrzemekKozakQA <przemyslaw.kozak.st@gmail.com>
2021-09-15 23:12:03 +0200

    Teoria z modułu 1

---
+++ src/MainApp.java
@@ -0,0 +1,39 @@
+public class MainApp {
+
+    public static void main(String[] args) {
+        //liczby całkowite
+        byte b;
+        short s;
+        int i;
+        long l;
+
+        int number; //deklaracja zmiennej
+        number = 10; // inicjalizacja zmiennej
+        System.out.println(number);
+
+        //liczby zmiennoprzecinkowe
+        float f;
+        double d;
+
+        double doubleNumber = 10.5; // deklaracja i inicjalizacja zmiennej jednocześnie
+        System.out.println(10.5);
+
+        //tekst
+        String someText = "text in java";
+        System.out.println(someText);
+
+        //znak
+        char someChar = 's';
+        System.out.println(someChar);
+
+        //prawda
+        Boolean trueValue = true;
+        System.out.println(trueValue);
+
+        //fałsz
+        Boolean falseValue = false;
+        System.out.println(falseValue);
+
+    }
+
+}
