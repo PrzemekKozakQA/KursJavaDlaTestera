9bbb44b9b1bb5e319e207d68e04be3deb35aea07
PrzemekKozakQA <przemyslaw.kozak.st@gmail.com>
2021-09-17 22:51:47 +0200

    homework nr 2 solution

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,53 +1,18 @@
 public class MainApp {
 
     public static void main(String[] args) {
+        Bug firstBug = new Bug("Błąd testowy", "autor@bug.com", 2);
 
-//        //TYPY DANYCH
-//
-//        //liczby całkowite
-//        byte b;
-//        short s;
-//        int i;
-//        long l;
-//
-//        int number; //deklaracja zmiennej
-//        number = 10; // inicjalizacja zmiennej
-//        System.out.println(number);
-//
-//        //liczby zmiennoprzecinkowe
-//        float f;
-//        double d;
-//
-//        double doubleNumber = 10.5; // deklaracja i inicjalizacja zmiennej jednocześnie
-//        System.out.println(10.5);
-//
-//        //tekst
-//        String someText = "text in java";
-//        System.out.println(someText);
-//
-//        //znak
-//        char someChar = 's';
-//        System.out.println(someChar);
-//
-//        //prawda
-//        Boolean trueValue = true;
-//        System.out.println(trueValue);
-//
-//        //fałsz
-//        Boolean falseValue = false;
-//        System.out.println(falseValue);
+        firstBug.showAllBugInfo();
 
-        //KLASY I OBIEKTY
+        firstBug.showBugReportAuthorEmail();
 
-        // Typ nazwa = nowy obiekt;
+        firstBug.showBugStatus();
 
-        User bartek = new User("Bartek", "Testowy", "bartek@test.pl", 17);
+        firstBug.bugPriority = firstBug.getBugPriority() + 1;
+        firstBug.bugStatus = true;
 
-        bartek.getAllInfo();
-
-        User tomek = new User("Tomek", "Brown", "tomek@brown.pl", 30);
-
-        tomek.getAllInfo();
+        firstBug.showAllBugInfo();
     }
 
 }
