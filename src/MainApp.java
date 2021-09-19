fc0f0e1c171783d3c79c0aefda7a0cc4ad897ae9
PrzemekKozakQA <przemyslaw.kozak.st@gmail.com>
2021-09-19 14:41:50 +0200

    solving homework 3

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,6 +1,24 @@
+import model.Bug;
+
 public class MainApp {
 
     public static void main(String[] args) {
+        Bug bug = new Bug("test", "test@test.pl", 1);
+
+        bug.setBugAuthorEmail("test@test");
+        bug.setBugAuthorEmail("testtest.pl");
+        bug.setBugAuthorEmail("testtestpl");
+        bug.setBugAuthorEmail("nowy@test.pl");
+        System.out.println("Email autora zgłoszenia: " + bug.getBugAuthorEmail());
+
+        bug.setBugDescription("123456789");
+        bug.setBugDescription("            ");
+        bug.setBugDescription(" ");
+        bug.setBugDescription("");
+        bug.setBugDescription("To jest nowy opis błędu");
+
+        System.out.println("Opis błędu: " + bug.getBugDescription());
+
 
     }
 
