3df9f297561216e66825e82891afd25da637d8c0
Przemyslaw.Kozak.QA <przemyslaw.kozak.qa@gmail.com>
2021-09-24 00:00:20 +0200

    homework5

--- src/MainApp.java
+++ src/MainApp.java
@@ -9,6 +9,8 @@ public class MainApp {
         Laptop gamingLaptop = new Laptop("XGames", "HP GAMES", 500, 256, 50);
 
         officeComputer.switchOn();
+        System.out.println(officeComputer.getState());
+
         officeComputer.setPowerSupply(true);
         officeComputer.switchOn();
         System.out.println(officeComputer.getState());
