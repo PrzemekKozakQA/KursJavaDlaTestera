1cfd848e0a654825d9910e3e54273f1d69f6f52a
Przemyslaw.Kozak.QA <przemyslaw.kozak.qa@gmail.com>
2021-09-30 18:48:21 +0200

    homework8 solution

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,24 +1,45 @@
 import model.Bug;
 import model.BugReporter;
-import model.User;
-import model.computer.*;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class MainApp {
 
     public static void main(String[] args) {
 
-        Map<Integer, String> names = new HashMap<>();
-        names.put(1, "kowalski1");
-        names.put(10, "kowalski2");
-        names.put(2, "kowalski3");
-        names.put(5, "kowalski4");
+        //new Bug list
+        List<Bug> bugList = new ArrayList<>();
 
-        for (Map.Entry<Integer, String> myEntry: names.entrySet()) {
-            System.out.println(myEntry.getKey()+" "+ myEntry.getValue());
+        bugList.add(new Bug("Opis testowy błędu nr 5", new BugReporter("Jan", "Nowak", "Jn@test.pl"), 1));
+        bugList.add(new Bug("Opis testowy błędu nr 5", new BugReporter("Jan", "Nowak", "Jn@test.pl"), 1));
+        bugList.add(new Bug("Opis testowy błędu nr 3", new BugReporter("Jan", "Nowak", "Jn@test.pl"), 1));
+        bugList.add(new Bug("Opis testowy błędu nr 2", new BugReporter("Jan", "Nowak", "Jn@test.pl"), 1));
+        bugList.add(new Bug("Opis testowy błędu nr 2", new BugReporter("Jan", "Nowak", "Jn@test.pl"), 1));
+        bugList.add(new Bug("Opis testowy błędu nr 1", new BugReporter("Jan", "Nowak", "Jn@test.pl"), 1));
+        bugList.add(new Bug("Opis testowy błędu nr 4", new BugReporter("Jan", "Nowak", "Jn@test.pl"), 1));
+        bugList.add(new Bug("Opis testowy błędu nr 1", new BugReporter("Jan", "Nowak", "Jn@test.pl"), 1));
+
+        System.out.println("List of bugs");
+        for (Bug bug : bugList) {
+            System.out.println(bug);
         }
+
+        //take unique value
+        Set<Bug> bugSet = new HashSet<>(bugList);
+
+        System.out.println("Set of unique bugs");
+        for (Bug bug : bugSet) {
+            System.out.println(bug);
+        }
+
+        System.out.println("Set of unique bugs sorted by description");
+        Set<Bug> bugSortedSet = new TreeSet<>(bugSet);
+
+
+        for (Bug bug : bugSortedSet) {
+            System.out.println(bug);
+        }
+
     }
 
 }
